e2879f7911d39ad55000a5cfd9997ffa95616951
liefu <liefu@flowingly.net>
2019-04-23 11:40:39 +1200

    initial commit

---
+++ ContosoUni/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUni.Models;
+using ContosoUniversity.Models;
+
+namespace ContosoUni.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUni.Models.SchoolContext _context;
+
+        public CreateModel(ContosoUni.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ContosoUni/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUni.Models;
+using ContosoUniversity.Models;
+
+namespace ContosoUni.Pages.Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUni.Models.SchoolContext _context;
+
+        public IndexModel(ContosoUni.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}
